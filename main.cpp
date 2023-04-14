f3e2904078a30e52b89f052ac313c6caa4f1e4e5
kins <k1dkins@yahoo.com>
2023-04-13 20:01:56 -0400

    feat: read text file and output to console

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(){
-	std::cout << "Hello World!" << std::endl;
+
+	// open file
+	std::ifstream file ("list.txt");
+
+	if ( file.is_open() ) {
+		char ch;
+		while ( file ) {
+			ch = file.get();
+			std::cout << ch;
+		}
+	} else {
+		std::cout << "Couldn't open file\n";
+	}
+	
 	return 0;
 }
